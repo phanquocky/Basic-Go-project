c18703b8a4cfff99c72c6d21e357625896702f24
Ky Phan <20120017@student.hcmus.edu.vn>
2023-10-07 13:47:19 +0700

    avoid deadlock

--- db/query/account.sql
+++ db/query/account.sql
@@ -29,6 +29,12 @@ WHERE id = $1
 RETURNING *;
 
 
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
+
 -- name: DeleteAccount :exec
 DELETE FROM accounts
 WHERE id = $1;

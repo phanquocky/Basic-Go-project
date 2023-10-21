179c131d9c956d80dd05ffeaff51cbfb271b2492
Ky Phan <20120017@student.hcmus.edu.vn>
2023-10-21 17:30:34 +0700

    create unit test for users, and correct handle db error for accounts

---
+++ db/sqlc/user_test.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"context"
+	"simplebank/util"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func CreateRandomUser(t *testing.T) User {
+	arg := CreateUserParams{
+		Username:       util.RandomOwner(),
+		HashedPassword: "secret",
+		FullName:       util.RandomOwner(),
+		Email:          util.RandomEmail(),
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, user.Username, arg.Username)
+	require.Equal(t, user.FullName, arg.FullName)
+	require.Equal(t, user.HashedPassword, arg.HashedPassword)
+	require.Equal(t, user.Email, arg.Email)
+
+	require.True(t, user.PasswordChangedAt.IsZero())
+	require.NotZero(t, user.CreatedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	CreateRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := CreateRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.Username)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.FullName, user2.FullName)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.Email, user2.Email)
+	require.Equal(t, user1.CreatedAt, user2.CreatedAt)
+	require.Equal(t, user1.PasswordChangedAt, user2.PasswordChangedAt)
+}
